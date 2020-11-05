8d468566e620717b7c31a491808774c826f00c13
rafiqul <rafiqul@scuti.asia>
2020-11-05 15:52:59 +0700

    update appspec.yml

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@
 #Auto run after deploy source code
 composer install && \
 composer dumpautoload && \
-sudo chmod -R 777 storage bootstrap/cache && \
-php artisan cache:clear && \
-php artisan config:clear && \
-php artisan route:clear && \
-php artisan view:clear
+sudo chmod -R 777 storage bootstrap/cache
